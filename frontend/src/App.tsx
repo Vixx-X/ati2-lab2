e8af40106c71b49e17bd4376cd1f82c08046fd90
EduhuSua <Eduardosuarez.ucv@gmail.com>
2022-08-26 00:06:43 -0400

    feat(creating notification sistem)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,19 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import LanguageSelect from "components/forms/LanguageSelect";
 import TaskTable from "components/tasks/TaskTable";
-
+import alertStore from "stores/AlertStore";
+import Alert from "@mui/material/Alert";
 const App = () => {
+  const alerts = alertStore((state)=>state.alerts)
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <TaskTable />
       <LanguageSelect />
+      {
+        alerts.map((alert)=>(
+          <Alert severity={alert.severity} key={alert.id}>{alert.text}</Alert>
+        ))
+      }
     </LocalizationProvider>
   );
 };
